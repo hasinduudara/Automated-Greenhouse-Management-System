312998673132b1595e44e006627f0111588eed76
hasinduudara <hasindutechschool@gmail.com>
2026-03-06 10:12:20 +0530

    add JWT authentication filter and configuration

---
+++ api-gateway/src/main/java/com/agms/api_gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.agms.api_gateway.filter;
+
+import com.agms.api_gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            // Check security for the request using the RouteValidator
+            if (validator.isSecured.test(exchange.getRequest())) {
+
+                // Check if the Authorization header is present
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete(); // 401 Error
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    // Check if the token is valid using JwtUtil
+                    jwtUtil.validateToken(authHeader);
+                } catch (Exception e) {
+                    // Token is invalid, return 401 Unauthorized
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+    }
+}
